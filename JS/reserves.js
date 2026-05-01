5d7e437c2e40e49698bb8b340b3bc460ec13d39b
DuckyProgramming <93790036+DuckyProgramming@users.noreply.github.com>
2026-05-01 09:29:10 -0400

    Sabratha

--- JS/reserves.js
+++ JS/reserves.js
@@ -8,5 +8,28 @@ reserves=[
             {level:3,type:`Infantry`,team:`Australian`,desc:`10th Infantry Battalion, 2nd Australian Expeditionary Force`,name:`2/10`,designation:``,commander:`Bruer`},
             {level:3,type:`Infantry`,team:`Australian`,desc:`12th Infantry Battalion, 2nd Australian Expeditionary Force`,name:`2/12`,designation:``,commander:`Field`},
         ],
+    },{
+        level:0,type:[`Infantry`],team:`Italian`,
+        desc:`60th Infantry Division 'Sabratha'`,name:`60`,designation:`Sabratha`,commander:`Soldarelli`,icon:`sabratha`,
+        pos:[480,441],
+        elements:[
+            {
+                level:2,type:[`Infantry`],team:`Italian`,
+                desc:`85th Infantry Regiment`,name:`85`,designation:`Sabratha`,commander:`Grimaldi`,icon:`sabratha`,
+                pos:[666,191],
+                elements:[
+                    {level:3,type:`Infantry`,team:`Italian`,desc:`1st Battalion, 85th Infantry Regiment`,name:`1`,designation:`85`,commander:`Bobbio`},
+                    {level:3,type:`Infantry`,team:`Italian`,desc:`2nd Battalion, 85th Infantry Regiment`,name:`2`,designation:`85`,commander:`Morosini`},
+                ],
+            },{
+                level:2,type:[`Infantry`],team:`Italian`,
+                desc:`86th Infantry Regiment`,name:`86`,designation:`Sabratha`,commander:`Castrignanò`,icon:`sabratha`,
+                pos:[2014,1167],
+                elements:[
+                    {level:3,type:`Infantry`,team:`Italian`,desc:`1st Battalion, 86th Infantry Regiment`,name:`1`,designation:`86`,commander:`Velosa`},
+                    {level:3,type:`Infantry`,team:`Italian`,desc:`2nd Battalion, 86th Infantry Regiment`,name:`2`,designation:`86`,commander:`Piana`},
+                ],
+            },
+        ],
     },
 ]
